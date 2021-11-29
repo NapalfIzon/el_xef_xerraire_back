8f9bb35a44667681350c33361f11f0e5dcd6d3e7
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-29 22:56:20 +0100

    :memo: Adding getRecipesById in recipesController

--- src/server/controller/recipesController.ts
+++ src/server/controller/recipesController.ts
@@ -1,4 +1,34 @@
-const getRecipe = () => {};
+import express from "express";
+import chalk from "chalk";
+import Debug from "debug";
+import Recipe from "../../database/models/recipe";
+import { RecipeSchema } from "../../interfaces/recipesInterface";
+
+const debug = Debug("xerrAPI:recipesController");
+
+const getRecipeById = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const { id } = req.body;
+
+  try {
+    const recipeData: RecipeSchema = await Recipe.findById(id);
+    debug(
+      chalk.bgGray.black(
+        `Se ha consultado la información de la receta ${id} ${"(´ ▽ `)b"}`
+      )
+    );
+
+    res.json(recipeData);
+  } catch {
+    const error: any = new Error(`No se ha encontrado la receta id: ${id}`);
+    error.code = 404;
+    error.status = 404;
+    next(error);
+  }
+};
 
 const searchRecipe = () => {};
 
@@ -11,7 +41,7 @@ const modifyRecipe = () => {};
 const removeRecipe = () => {};
 
 export {
-  getRecipe,
+  getRecipeById,
   searchRecipe,
   addRecipe,
   uploadVote,
