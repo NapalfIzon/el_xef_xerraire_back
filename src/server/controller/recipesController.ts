c457492ba5044f84b269bf82d1fffc228ef50087
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-29 20:57:11 +0100

    :arrow_up: Adding initial empty recipesController

---
+++ src/server/controller/recipesController.ts
@@ -0,0 +1,20 @@
+const getRecipe = () => {};
+
+const searchRecipe = () => {};
+
+const addRecipe = () => {};
+
+const uploadVote = () => {};
+
+const modifyRecipe = () => {};
+
+const removeRecipe = () => {};
+
+export {
+  getRecipe,
+  searchRecipe,
+  addRecipe,
+  uploadVote,
+  modifyRecipe,
+  removeRecipe,
+};
