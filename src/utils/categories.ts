09bcac820a12fabac883eed33c2bb76db476cd56
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-12-01 15:50:46 +0100

    :arrow_up: Adding categories list

---
+++ src/utils/categories.ts
@@ -0,0 +1,27 @@
+const categories: Array<string> = [
+  "Aperitivos",
+  "Tapas",
+  "Arroces",
+  "Cereales",
+  "Aves",
+  "Carne",
+  "Cócteles",
+  "Bebidas",
+  "Ensaladas",
+  "Guisos",
+  "Huevos",
+  "Lácteos",
+  "Legumbres",
+  "Mariscos",
+  "Pan",
+  "Bollería",
+  "Pasta",
+  "Pescado",
+  "Postres",
+  "Salsas",
+  "Sopas",
+  "Cremas",
+  "Verduras",
+];
+
+export default categories;
